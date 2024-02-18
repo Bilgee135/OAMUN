e49d4feeb4a8d4266318513aac8de70e9a7a22dc
Allen <bilegsaihan2006@gmail.com>
2024-02-18 23:56:04 +0800

    navbar shu

--- src/index.php
+++ src/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OAMUN</title>
+    <link rel="stylesheet" href="styles/index.css">
 </head>
+
 <body>
-    
+    <div class="hero-section">
+        <?php
+        include 'navbar.php';
+        ?>
+        Hello
+    </div>
+
 </body>
+
 </html>
