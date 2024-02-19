6c680eb97db6465a4e562cb082f7fae1cc0402ee
Bilegsaikhan <bilegsaihan2006@gmail.com>
2024-02-19 10:59:28 +0800

    Registration complete

--- src/register.php
+++ src/register.php
@@ -20,19 +20,36 @@ if(isset($_POST['submit'])){
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Forum</title>
+    <link rel="stylesheet" href="styles/registration.css">
+</head>
+<body>
+<?php
+include 'bootstrap.php';
+include 'navbar.php';
+?>
+<div class="page">
+    <div class="forum h-auto rounded bg-white">
+        <div class="forum-head rounded">
+            <h1 class="text-white text-center">OAMUN Registration Forum</h1>
+        </div>
+        <form method="POST">
+        <input name="firstname" type="text" placeholder="First name" class="form-control mt-3" required>
+        <input name="lastname" type="text" placeholder="Last name" class="form-control mt-3" required>
+        <input name="email" type="email" placeholder="Email address" class="form-control mt-3" required>
+        <input name="school" type="text" placeholder="Current school" class="form-control mt-3" required>
+        <input name="class" type="text" placeholder="Grade" class="form-control mt-3" required>
+        <textarea name="reason" type="text" placeholder="Why do you want to participate?" class="form-control mt-3" required></textarea>
+        <button name="submit" class="btn btn-primary w-100 mt-3" type="submit" value="Register">Register</button>
+        </form>
+    </div>
+</div>
 
-<form method="POST">
-<label>First Name:</label>
-<input name="firstname" type="text" required>
-<label>Last Name:</label>
-<input name="lastname" type="text" required>
-<label>Email Address:</label>
-<input name="email" type="email" required>
-<label>Current School:</label>
-<input name="school" type="text" required>
-<label>Grade:</label>
-<input name="class" type="text" required>
-<label>Why are u interested? (in 3 words):</label>
-<input name="reason" type="text" required>
-<button name="submit" type="submit" value="Submit">Оруулах</button>
 </form>
+</body>
+</html>
