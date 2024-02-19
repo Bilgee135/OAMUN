75cf68c9b65ef18405bd5cbd09de2109ddfc4f36
badruulT <badruultugsbayar8v@gmail.com>
2024-02-19 10:33:57 +0800

    Position Paper Guide Page Complete!

---
+++ src/ppg.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OAMUN</title>
+    <link rel="stylesheet" href="./styles/ppg.css">
+</head>
+
+<body>
+    <?php
+    include './navbar.php';
+    ?>
+    <div class="hero-section"></div>
+    <div class="container-1 row">
+        <div class="container-2 col-lg-6">
+            <div style="width: 100%; padding-left: 10%; padding-right: 10%" class="w-100">
+                <hr style="border-color: black; margin-bottom: 30px"/>
+                <p class="rubik" style="font-size: 25px;">Position Paper Guide</p>
+                <hr style="border-color: black; margin-top: 30px"/>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-3 row" style="display: flex; justify-content: center; padding-left: 10%; padding-right: 10%">
+        <div class="container-4 col-lg-8" style="height: auto; margin-bottom: 30px">
+            <p style="font-size: 20px;"><b>Position Paper Overview</b></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum laoreet massa, blandit tempus ex mattis sed. Curabitur pellentesque, felis in lacinia commodo, turpis nunc rhoncus nunc, ullamcorper auctor dui dolor vitae nisi. Duis imperdiet porta bibendum. Aliquam pellentesque commodo molestie.</p>
+            <p style="font-size: 20px; margin-top: 70px"><b>Position Paper Formatting</b></p>
+        </div>
+        <div class="paperformatimg"></div>
+    </div>
+</body>
+
+</html>
