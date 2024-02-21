00e156aabebe829a80c262fa008d0e96053b3a56
Sugar-Ochir Etseggui <sugarochir2k@gmail.com>
2024-02-22 01:31:59 +0800

    footer responsive sdfjak

--- src/footer.php
+++ src/footer.php
@@ -1,30 +1,56 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<link rel="stylesheet" href="styles/footer.css">
-
-<footer>
-    <div class="container">
-        <section class="sec quicklinks">
-            <h2>Conference Information</h2>
-            <p><i class="fa-regular fa-calendar"></i> March 16, 2024</p>
-            <p><i class="fa-solid fa-location-dot"></i> Olonlog Academy, Ikh Khuree St, Ulaanbaatar 13312</p>
-            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2674.7753503642116!2d106.9351008764097!3d47.90203327121849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5d96922b52bbc087%3A0x8b7b96cfac760f98!2sOlonlog%20Academy!5e0!3m2!1sen!2smn!4v1708273120551!5m2!1sen!2smn" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade" loading="lazy" frameborder="0" title="Conference location on Google Maps" style="width: 100%; height: auto;"></iframe>
-        </section>
-        <section class="sec aboutus">
-            <h2>About Us</h2>
-            <p>OAMUN is a conference led by Olonlog IB Year 2 students. We consist of 10 members, each with their own respective roles.</p>
-            <ul class="sci">
-                <li><a href="https://www.facebook.com/olonlog/"><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href="https://www.youtube.com/@olonlogacademy939"><i class="fa-brands fa-youtube"></i></a></li>
-            </ul>
-        </section>
-        <section class="sec contact">
-            <h2>Contact Us</h2>
-            <p><strong>Email:</strong> oas20171667@olonlog.edu.mn</p>
-            <p><strong>Phone:</strong> +976 9923 4704</p>
-        </section>
-    </div>
-    <hr>
-    <p class="cr">© Olonlog Academy School</p>
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Footer Design</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles/footer2.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>company</h4>
+  	 			<ul>
+  	 				<li><a href="#">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 				<li><a href="#">affiliate program</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>get help</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">order status</a></li>
+  	 				<li><a href="#">payment options</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+      
+</body>
+</html>
