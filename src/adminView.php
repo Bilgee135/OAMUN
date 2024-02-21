2adfb49e2619874a120ab2796034ba1ee41179d6
Bilegsaikhan <bilegsaihan2006@gmail.com>
2024-02-21 14:50:22 +0800

    Admin Page done

--- src/adminView.php
+++ src/adminView.php
@@ -1,11 +1,67 @@
+<?php
+
+session_start();
+
+include 'bootstrap.php';
+include 'dbConnect.php';
+
+if (!isset($_SESSION['admin'])) {
+    // Redirect to index.php
+    header("Location: index.php");
+    exit();
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Admin!</title>
+    <link rel="stylesheet" href="styles/adminView.css">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
 </head>
 <body>
+    <?php
+    include 'adminNavbar.php';
+    ?>
+    <div class="container">
+    <table class="table mt-5 table-striped-columns table-hover">
+    <thead class="table-dark">
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Firstname</th>
+        <th scope="col">Lastname</th>
+        <th scope="col">School</th>
+        <th scope="col">Grade</th>
+        <th scope="col">Reason</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $select = "SELECT * FROM `registration`";
+        $result = mysqli_query($conn, $select);
+        while($print = mysqli_fetch_assoc($result)) {
+            ?>
+            <tr>
+            <th scope="row"><?php echo $print['id'] ?></th>
+            <td><?php echo $print['firstname'] ?></td>
+            <td><?php echo $print['lastname'] ?></td>
+            <td><?php echo $print['school'] ?></td>
+            <td><?php echo $print['class'] ?></td>
+            <td><?php echo $print['reason'] ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+        
+        
+    </tbody>
+    </table>
+    </div>
     
 </body>
 </html>
--- src/control.php
+++ src/control.php
@@ -4,9 +4,14 @@ include 'dbConnect.php';
 include 'bootstrap.php';
 
 if(isset($_POST['access'])) {
-    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // $re_password = $_POST['repassword'];
 }
 
+function AdminCredentials($username, $password) {
+    return ($username == "Yale" && $password == "OAMUN");
+}
 ?>
 
 
@@ -20,12 +25,31 @@ if(isset($_POST['access'])) {
     <link rel="icon" type="image/x-icon" href="../favicon.ico">
 </head>
 <body class="container">
-    <form class="form-control mt-5 px-5 py-2">
+    <form class="form-control mt-5 mb-3 px-5 py-2" method="POST" action="">
         <h1 class="fs-2 text-dark my-2">Control Board Access</h1>
-        <input class="form-control mt-2" placeholder="Username" name="username" type="text">
+        <input class="form-control mt-3" placeholder="Username" name="username" type="text">
         <input class="form-control mt-3" placeholder="Password" name="password" type="password">
-        <input class="form-control mt-3" placeholder="Re-enter password" name="repassword" type="password">
-        <button class="btn btn-primary w-100 my-3" name="access" type="submit">Access</button>
+        <!-- <input class="form-control mt-3" placeholder="Re-enter password" name="repassword" type="password"> -->
+        <input type="submit" class="btn btn-primary w-100 my-3" name="access" value="Access">
     </form>
+    <div>
+        <?php
+            if(AdminCredentials($username, $password)) {
+                
+                $_SESSION['admin'] = true;
+        
+                // Redirect to adminView.php
+                header("Location: adminView.php");
+                exit();
+            } else {
+                $_SESSION['admin'] = false;
+                unset($_SESSION['admin']);
+                echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        Invalid username or password!
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                      </div>';
+            }
+        ?>
+    </div>
 </body>
 </html>
